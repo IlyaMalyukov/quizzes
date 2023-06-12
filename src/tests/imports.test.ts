2ad5ab3ac5a65ba276e14788220540301b015367
Ilya Malyukov <ilya.maliukov@yandex.ru>
2023-06-12 14:24:56 +0300

    Структура файлов по feature-sliced design

--- src/tests/imports.test.ts
+++ src/tests/imports.test.ts
@@ -1,18 +1,5 @@
-describe('import vue components', () => {
-    test('normal imports as expected', async () => {
-        const cmp = await import('../pages/main-page')
-        expect(cmp).toBeDefined()
-    })
+import { expect, test } from 'vitest'
 
-    test('template string imports as expected', async () => {
-        // eslint-disable-next-line @typescript-eslint/quotes
-        const cmp = await import(`../pages/main-page`)
-        expect(cmp).toBeDefined()
-    })
-
-    test('dynamic imports as expected', async () => {
-        const name = 'main-page'
-        const cmp = await import(`../pages/${name}`)
-        expect(cmp).toBeDefined()
-    })
+test('should work as expected', () => {
+    expect(Math.sqrt(4)).toBe(2)
 })
