66b000ad73442ec53c62c6e7eff26d248602cd09
Ilya Malyukov <ilya.maliukov@yandex.ru>
2023-06-17 22:49:52 +0300

    установка и настройка axios

--- src/pages/main-page/ui.vue
+++ src/pages/main-page/ui.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
   import { useMainPage } from "./lib";
+  import { axios } from '../../shared';
 
   const { count } = useMainPage();
+
+  axios({
+    method: 'GET',
+    params: {
+      limit: 10,
+    },
+  })
 </script>
 
 <template>
