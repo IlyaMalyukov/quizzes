1a208e5730ca4cb2b6071ab5ff9a8f3ac30a7d99
Ilya Malyukov <ilya.maliukov@yandex.ru>
2023-06-17 22:47:48 +0300

    настройка алиасов

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -2,11 +2,17 @@
 
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
     plugins: [
         Vue(),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    },
     test: {
         globals: true,
         environment: 'jsdom',
