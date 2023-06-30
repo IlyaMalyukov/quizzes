8cce85170fd97a9123d3db966669044984991c65
imalyukov <imalyukov@phoenixit.com>
2023-06-30 16:10:51 +0300

    Установка и подключение Vuetify

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import './style.css'
 import App from './ui.vue'
 
@@ -8,8 +14,13 @@ import { worker } from "@/__mocks__";
 await worker.start();
 
 const pinia = createPinia();
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 const app = createApp(App);
 
 app.use(pinia)
+app.use(vuetify)
 app.mount('#app')
