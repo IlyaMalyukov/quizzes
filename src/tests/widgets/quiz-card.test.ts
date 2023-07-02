6e40337d0a934ee04499ff78b0b0bff87e2bd551
imalyukov <imalyukov@phoenixit.com>
2023-07-02 23:22:55 +0300

    Тест mount component для карточки квиза

--- src/tests/widgets/quiz-card.test.ts
+++ src/tests/widgets/quiz-card.test.ts
@@ -1,16 +1,19 @@
-import {describe, test, expect } from 'vitest'
+import { describe, test, expect } from 'vitest'
 import { mount } from '@vue/test-utils';
 
 import { QuizCard } from '@/widgets';
 
 
 describe('Quiz Card', () => {
-    test('renders properly', () => {
-        const wrapper = mount(QuizCard, {
-            props: {
-                quiz: { id: 0, title: 'Заголовок' }
-            },
-        });
+    const wrapper = mount(QuizCard, {
+        props: {
+            quiz: { id: 0, title: 'Title' }
+        }
+    });
+
+    test('mount component', async () => {
+        expect(QuizCard).toBeTruthy();
+    });
 
         expect(wrapper.text()).toContain('Заголовок');
     })
