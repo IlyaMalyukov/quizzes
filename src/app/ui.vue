f553477094c2d3c39e0ae945b5945b40c5ff00b4
imalyukov <imalyukov@phoenixit.com>
2023-07-03 14:17:59 +0300

    расположение контента от верха страницы

--- src/app/ui.vue
+++ src/app/ui.vue
@@ -1,3 +1,15 @@
 <template>
   <router-view />
 </template>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+</style>
+
