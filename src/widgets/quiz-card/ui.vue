efe0c468f84382469f6415bfa08c8ff184967fbc
imalyukov <imalyukov@phoenixit.com>
2023-07-03 14:18:07 +0300

    расположение контента от верха страницы

--- src/widgets/quiz-card/ui.vue
+++ src/widgets/quiz-card/ui.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
 import { PropType } from "vue";
+import { useRouter } from 'vue-router';
 
 type QuizCard = { id: Number, title: String };
 
 defineProps({
   quiz: Object as PropType<QuizCard>
 });
+
+const router = useRouter();
+
+const openQuiz = (id) => {
+  router.push({ name: 'quiz-page', query: { id } });
+}
 </script>
 
 <template>
   <v-card :title="quiz.title" theme="dark" class="mb-5">
     <template #actions>
-      <v-btn variant="text" color="#3CB371">Пройти квиз</v-btn>
+      <v-btn @click="openQuiz(quiz.id)" variant="text" color="#3CB371">Пройти квиз</v-btn>
     </template>
   </v-card>
 </template>
