376345b051cb5968395940405179d86d226f6e72
imalyukov <imalyukov@phoenixit.com>
2023-07-22 23:34:31 +0300

    Переход на страницу квиза через router-link

--- src/entities/quiz-card/ui.vue
+++ src/entities/quiz-card/ui.vue
@@ -1,24 +1,19 @@
 <script setup lang="ts">
-import { PropType } from "vue";
-import { useRouter } from 'vue-router';
+import { PropType } from 'vue';
 
 type QuizCard = { id: Number, title: String };
 
 defineProps({
   quiz: Object as PropType<QuizCard>
 });
-
-const router = useRouter();
-
-const openQuiz = (id) => {
-  router.push({ name: 'quiz-page', query: { id } });
-}
 </script>
 
 <template>
   <v-card :title="quiz.title" theme="dark" class="mb-5">
     <template #actions>
-      <v-btn @click="openQuiz(quiz.id)" variant="text" color="#3CB371">Пройти квиз</v-btn>
+      <router-link :to="{ name: 'quiz-page', query: { id: quiz.id } }">
+        <v-btn variant="text" color="#3CB371">Пройти квиз</v-btn>
+      </router-link>
     </template>
   </v-card>
 </template>
