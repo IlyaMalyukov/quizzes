1639a40c8d443c4cd03fac84d38b74cdc6979295
imalyukov <imalyukov@phoenixit.com>
2023-07-23 00:54:28 +0300

    тест перехода на страницу квиза при клике на кнопку

--- src/entities/quiz-card/quiz-card.test.ts
+++ src/entities/quiz-card/quiz-card.test.ts
@@ -1,6 +1,7 @@
 import { describe, test, expect } from 'vitest'
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount, flushPromises } from '@vue/test-utils';
 
+import { router } from '@/app/router'
 import { QuizCard } from '@/entities';
 
 
@@ -20,4 +21,39 @@ describe('Quiz Card', () => {
         expect(wrapper.props().quiz.id).toBe(0);
         expect(wrapper.props().quiz.title).toBe('New super quiz');
     });
+
+    // test('Router renders single quiz page when clicking a button', async () => {
+    //     await router.push('/');
+    //     await router.isReady();
+    //
+    //     const wrapper = mount(QuizCard, {
+    //         global: {
+    //             plugins: [router],
+    //         },
+    //         propsData: {
+    //             quiz: { id: 0, title: 'New super quiz' }
+    //         },
+    //     });
+    //
+    //     expect(wrapper.html()).toMatch('Пройти квиз');
+    //     await wrapper.find('a').trigger('click');
+    //     await flushPromises();
+    //     expect(wrapper.text()).not.toMatch('Пройти квиз');
+    // });
+
+    // test('123', async () => {
+    //     await router.replace('/');
+    //     await router.push('/quiz?id=0');
+    //
+    //     const wrapper = mount(QuizCard, {
+    //         global: {
+    //             plugins: [router],
+    //         },
+    //         propsData: {
+    //             quiz: { id: 0, title: 'New super quiz' }
+    //         },
+    //     });
+    //
+    //     expect(wrapper.text()).not.toMatch('Пройти квиз');
+    // });
 });
