aaad5ed7be85f9b63f8050b755a11ba102f8d3ca
imalyukov <imalyukov@phoenixit.com>
2023-07-23 01:16:20 +0300

    удалил лишний тест

--- src/pages/main-page/main-page.test.ts
+++ src/pages/main-page/main-page.test.ts
@@ -2,17 +2,11 @@ import { beforeEach, describe, expect, test } from 'vitest'
 import { createPinia, setActivePinia } from 'pinia';
 import { shallowMount } from '@vue/test-utils';
 
-import { MainPage, useMainPage } from '@/pages/main-page'
+import { MainPage } from '@/pages/main-page'
 
 describe('Main Page', () => {
     beforeEach(() => {
         setActivePinia(createPinia());
-    });
-
-    test('Expected list to be defined', () => {
-        const { quizzesList } = useMainPage();
-
-        expect(quizzesList).toBeDefined();
     });
 
     test('Main Page renders title correctly', () => {
