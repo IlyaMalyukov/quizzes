45fcf31cfa99e99fe8192d54174024915f23279d
imalyukov <imalyukov@phoenixit.com>
2023-07-30 00:51:00 +0300

    вывод кол-ва очков

--- src/entities/result-card/ui.vue
+++ src/entities/result-card/ui.vue
@@ -3,12 +3,14 @@ import { defineProps, PropType } from 'vue';
 import type { Result } from './lib/types';
 
 defineProps({
-  result: Object as PropType<Result>
+  result: Object as PropType<Result>,
+  points: Number,
 });
 </script>
 
 <template>
   <div class="card">
-    <h3> {{ result.value }} </h3>
+    <h3> {{ result?.value }} </h3>
+    <p> Набрано очков: {{ points }} </p>
   </div>
 </template>
