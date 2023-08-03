8ccb3c3a3f22c8a7568281dea764c8e188351691
imalyukov <imalyukov@phoenixit.com>
2023-08-03 17:35:47 +0300

    вывод картинки и кнопки "назад" в карточке результата

--- src/entities/result-card/ui.vue
+++ src/entities/result-card/ui.vue
@@ -10,5 +10,25 @@ defineProps({
 <template>
   <div class="card">
     <h3> {{ result?.value }} </h3>
+    <img :src="result.img" alt="img"/>
+    <router-link to="/">
+      <button> Назад </button>
+    </router-link>
   </div>
 </template>
+
+<style scoped>
+.card {
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+img {
+  margin: 10px 0;
+  max-width: 250px;
+  border-radius: 10px;
+  box-shadow: 15px 18px 5px -5px rgba(34, 34, 34, 0.6);
+}
+</style>
