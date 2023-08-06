5176d35dcaab49af45ef57b4a1dd14f2c70c8a6d
imalyukov <imalyukov@phoenixit.com>
2023-08-06 23:58:23 +0300

    тест router-link в карточке результата

--- src/entities/result-card/result-card.test.ts
+++ src/entities/result-card/result-card.test.ts
@@ -1,10 +1,21 @@
-import { describe, test, expect } from 'vitest'
-import { shallowMount } from '@vue/test-utils';
+import { describe, test, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import { ResultCard } from './index';
 
 describe('Result Card', () => {
-  const wrapper = shallowMount(ResultCard, {
+  const routes = [
+    { path: '/', component: { template: '<div>Назад</div>' } }
+  ];
+
+  const router = createRouter({
+    history: createWebHistory(),
+    routes
+  });
+
+  const wrapper = mount(ResultCard, {
+    global: { plugins: [router] },
     propsData: {
       result: { id: 0, value: 'Sorry you’re not a winner' },
     },
@@ -19,5 +30,12 @@ describe('Result Card', () => {
 
     expect(props.result.id).toBe(0);
     expect(props.result.value).toBe('Sorry you’re not a winner');
+  });
+
+  test('Render router-link to "Назад"', () => {
+    const link = wrapper.find('a');
+
+    expect(link.text()).toBe('Назад');
+    expect(link.attributes('href')).toBe('/');
   });
 });
